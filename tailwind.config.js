e0fc2c073cc32ac8ba6d5cf79e31b678b2050667
Syed-Ali-Jawad <alijawad04@gmail.com>
2024-06-08 18:27:56 +0500

    adding project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "intro-bg": "url('/build/img/xbg_3.png')",
+        "about-bg": "url('/build/img/xbg_2.png')",
+        "footer-bg": "url('/build/img/stethosocpe bg.jpg')",
+      },
+      screens: {
+        md: { max: "767px" },
+      },
+    },
+    plugins: [],
+  },
+};
