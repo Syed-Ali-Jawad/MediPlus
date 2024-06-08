62f90769592accd3c0044a0cd93bfdb8dda06b48
Syed-Ali-Jawad <alijawad04@gmail.com>
2024-06-08 19:39:11 +0500

    deployment adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "intro-bg": "url('../img/xbg_3.png')",
-        "about-bg": "url('../img/xbg_2.png')",
-        "footer-bg": "url('../img/stethosocpe bg.jpg')",
+        "intro-bg": "url('build/img/xbg_3.png')",
+        "about-bg": "url('build/img/xbg_2.png')",
+        "footer-bg": "url('build/img/stethosocpe bg.jpg')",
       },
       screens: {
         md: { max: "767px" },
